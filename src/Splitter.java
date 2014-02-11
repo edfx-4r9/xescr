e5041733867841a26a974bd0aabd4754f6d873a1
Your Name <you@example.com>
2014-02-11 15:08:51 +0200

    Splitter component rewritten using inner class

--- src/Splitter.java
+++ src/Splitter.java
@@ -3,7 +3,6 @@ package com.edifecs.etools.xeserver.component.splitter;
 import java.util.Map;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.util.HashMap;
 
 import java.lang.IllegalArgumentException;
 import org.apache.commons.io.IOUtils;
@@ -15,157 +14,183 @@ import com.edifecs.etools.route.api.IProcessingContext;
 import com.edifecs.etools.route.api.ProcessingException;
 import com.edifecs.etools.route.api.integration.IProcessor;
 
-public class Splitter implements IProcessor, SplitterCallback {
-	public static final String MD_SPLIT_MESSAGE_ID = "SPLIT_MESSAGE_ID";
-	public static final String MD_SPLIT_MESSAGE_COUNT = "SPLIT_MESSAGE_COUNT";
-	public static final String MD_SPLIT_MESSAGE_LAST = "SPLIT_MESSAGE_LAST";
-	public static final String MD_SPLIT_CORRELATION_ID = "SPLIT_CORRELATION_ID";
+public class Splitter implements IProcessor
+{
+    public static final String MD_SPLIT_MESSAGE_ID      = "SPLIT_MESSAGE_ID";
+    public static final String MD_SPLIT_MESSAGE_COUNT   = "SPLIT_MESSAGE_COUNT";
+    public static final String MD_SPLIT_MESSAGE_LAST    = "SPLIT_MESSAGE_LAST";
+    public static final String MD_SPLIT_CORRELATION_ID  = "SPLIT_CORRELATION_ID";
 
-	public static final String SUPPRESS_EMPTY_MESSAGES = "SuppressEmptyMessages";
-	public static final String RECORD_SEPARATOR = "RecordSeparator";
-	public static final String DEFAULT_RECORD_SEPARATOR = "0x0D, 0x0A";
-	public static final String MD_SPLIT_MESSAGE_LENGTH = "SPLITTED_MESSAGE_LENGTH";
+    public static final String SUPPRESS_EMPTY_MESSAGES  = "SuppressEmptyMessages";
+    public static final String RECORD_SEPARATOR         = "RecordSeparator";
+    public static final String DEFAULT_RECORD_SEPARATOR = "0x0D, 0x0A";
+    public static final String MD_SPLIT_MESSAGE_LENGTH  = "SPLITTED_MESSAGE_LENGTH";
 
-	private boolean splitByMessage = true;
-	private boolean suppressEmptyMessages = false;
-	// private int nMessageCounter;
+    public Splitter(Map<String, String> configuration)
+    {
+    }
 
-//	private HashMap<Splitter, Map<String, Object>> mapper_headers = new HashMap<Splitter, Map<String, Object>>();
-//	private HashMap<Splitter, IProcessingContext> mapper_context = new HashMap<Splitter, IProcessingContext>();
-	private HashMap<Splitter, MapperUnit> mapper_unit = new HashMap<Splitter, MapperUnit>();
+    @Override
+    public void process(IProcessingContext context) throws ProcessingException
+    {
+        IMessage[] messages = context.getInputMessage().getMessages();
 
-	public Splitter(Map<String, String> configuration) {
-	}
+        for (int i = 0; i < messages.length; i++)
+        {
 
-	@Override
-	public void process(IProcessingContext context) throws ProcessingException {
-		IMessage[] messages = context.getInputMessage().getMessages();
-//		mapper_context.put(this, context);
-		mapper_unit.put(this, new MapperUnit());
-		mapper_unit.get(this).context = context;
+            String suppressEmptyMessagesString = context.getContextProperties().get(SUPPRESS_EMPTY_MESSAGES);
+            boolean suppressEmptyMessages = suppressEmptyMessagesString != null && Boolean.valueOf(suppressEmptyMessagesString);
+            String recSepHexString = context.getContextProperties().get(RECORD_SEPARATOR);
+            boolean splitByMessage = recSepHexString == null;
 
-		String suppressEmptyMessagesString = context.getContextProperties()
-				.get(SUPPRESS_EMPTY_MESSAGES);
-		suppressEmptyMessages = suppressEmptyMessagesString != null
-				&& Boolean.valueOf(suppressEmptyMessagesString);
-		String recSepHexString = context.getContextProperties().get(
-				RECORD_SEPARATOR);
-		if (recSepHexString == null) {
-			splitByMessage = true;
-		} else {
-			splitByMessage = false;
-		}
+            //            TODO - REMOVE
+            if (recSepHexString == null)
+            {
+                splitByMessage = true;
+            }
+            else
+            {
+                splitByMessage = false;
+            }
 
-		for (int i = 0; i < messages.length; i++) {
-			Map<String, Object> msgHeaders = context.getInputMessage()
-					.getMessages()[i].getHeaders();
 
-			IMessage message = messages[i];
-			int nMessageID = i + 1;
-			Map<String, Object> md = message.getMessageDescriptor();
-			md.put(MD_SPLIT_MESSAGE_ID, String.valueOf(nMessageID));
-			md.put(MD_SPLIT_MESSAGE_COUNT, messages.length);
-			md.put(MD_SPLIT_MESSAGE_LAST,
-					String.valueOf(nMessageID == messages.length));
-			md.put(MD_SPLIT_CORRELATION_ID, context.getInputMessage().getID());
-			if (!suppressEmptyMessages || message.getBodySize() > 0) {
-				if (splitByMessage) {
-					context.putResult(message);
-				} else {
-//					mapper_headers.put(this, msgHeaders);
-					mapper_unit.get(this).nMessageCounter = 0;
-					mapper_unit.get(this).headers = msgHeaders;
-					byte[] recSepBytes = separatorHexToBytes(recSepHexString);
-					msgHeaders.put(RECORD_SEPARATOR, recSepHexString);
-					// nMessageCounter = 0;
+            Map<String, Object> msgHeaders = context.getInputMessage().getMessages()[i].getHeaders();
 
-					StreamSplitter wrk = new StreamSplitter();
-					try (java.io.BufferedInputStream inputStream = new java.io.BufferedInputStream(
-							message.getBodyAsStream())) {
-						wrk.splitMessageByRecords(inputStream, recSepBytes,
-								this);
-					} catch (Exception e) {
-						throw new ProcessingException(e);
-					} finally {
-						// remove from map
-//						mapper_headers.remove(this);
-					}
-				}
-			}
-		}
-//		mapper_context.remove(this);
-		mapper_unit.remove(this);
-	}
+            IMessage message = messages[i];
+            int nMessageID = i + 1;
+            Map<String, Object> md = message.getMessageDescriptor();
+            md.put(MD_SPLIT_MESSAGE_ID, String.valueOf(nMessageID));
+            md.put(MD_SPLIT_MESSAGE_COUNT, messages.length);
+            md.put(MD_SPLIT_MESSAGE_LAST, String.valueOf(nMessageID == messages.length));
+            md.put(MD_SPLIT_CORRELATION_ID, context.getInputMessage().getID());
+            if (!suppressEmptyMessages || message.getBodySize() > 0)
+            {
+                if (splitByMessage)
+                {
+                    context.putResult(message);
+                }
+                else
+                {
+                    SplitterCallback cb = new SplitterCallback(context, message);
+                    cb.setRrecordSeparator(recSepHexString);
+                    byte[] recSepBytes = separatorHexToBytes(recSepHexString);
 
-	private byte[] separatorHexToBytes(String separatorHexString)
-			throws ProcessingException {
-		byte[] recSep;
-		if (separatorHexString.length() == 0) {
-			separatorHexString = DEFAULT_RECORD_SEPARATOR;
-		}
-//		mapper_headers.get(this).put(RECORD_SEPARATOR, separatorHexString);
-		mapper_unit.get(this).headers.put(RECORD_SEPARATOR, separatorHexString);
-		String[] recSepBytes = separatorHexString.split("[ ,]+");
-		recSep = new byte[recSepBytes.length];
-		try {
-			for (int i = 0; i < recSepBytes.length; i++) {
-				recSep[i] = Integer.decode(recSepBytes[i]).byteValue();
-			}
-		} catch (Exception e) {
-			throw new ProcessingException("Incorrect record separator "
-					+ separatorHexString);
-		}
-		return recSep;
-	}
+                    StreamSplitter wrk = new StreamSplitter();
+                    try (java.io.BufferedInputStream inputStream = new java.io.BufferedInputStream(message.getBodyAsStream()))
+                    {
+                        wrk.splitMessageByRecords(inputStream, recSepBytes, cb);
+                    }
+                    catch (Exception e)
+                    {
+                        throw new ProcessingException(e);
+                    }
+                    finally
+                    {
+                    }
+                }
+            }
+        }
+    }
 
-	@Override
-	public void pushMessageCallBack(OutputStream msgOutput) throws IOException {
-		try {
-			if (!(msgOutput instanceof SmartStream)) {
-				throw new IllegalArgumentException(
-						"SmartStream type expected for message creation");
-			} else {
-//				Map<String, Object> msgHeaders = mapper_headers.get(this);
-//				IProcessingContext cntxt = mapper_context.get(this);
-				Map<String, Object> msgHeaders = mapper_unit.get(this).headers;
-				IProcessingContext cntxt = mapper_unit.get(this).context;
-				if (!suppressEmptyMessages
-						|| ((SmartStream) msgOutput).getByteCount() > 0) {
-					// ++nMessageCounter;
-					// msgHeaders.put(MD_SPLIT_MESSAGE_ID, nMessageCounter);
-					++mapper_unit.get(this).nMessageCounter;
-					msgHeaders.put(MD_SPLIT_MESSAGE_ID, mapper_unit.get(this).nMessageCounter);
-					msgHeaders.put(MD_SPLIT_MESSAGE_LENGTH,
-							((SmartStream) msgOutput).getByteCount());
-					IMessage msgProcessed = cntxt.getMessageFactory()
-							.createMessage(msgHeaders,
-									((SmartStream) msgOutput).getInputStream());
-					ICompositeMessage msgExProcessed = cntxt
-							.getMessageFactory().createCompositeMessage(
-									msgHeaders, msgProcessed);
-					cntxt.putResult(msgExProcessed);
-				}
-			}
-		} catch (Exception e) {
-			throw new IOException(e.getCause());
-		}
+    private byte[] separatorHexToBytes(String separatorHexString) throws ProcessingException
+    {
+        byte[] recSep;
+        if (separatorHexString.length() == 0)
+        {
+            separatorHexString = DEFAULT_RECORD_SEPARATOR;
+        }
+        String[] recSepBytes = separatorHexString.split("[ ,]+");
+        recSep = new byte[recSepBytes.length];
+        try
+        {
+            for (int i = 0; i < recSepBytes.length; i++)
+            {
+                recSep[i] = Integer.decode(recSepBytes[i]).byteValue();
+            }
+        }
+        catch (Exception e)
+        {
+            throw new ProcessingException("Incorrect record separator " + separatorHexString);
+        }
+        return recSep;
+    }
 
-	}
+    //    @Override
+    public void pushMessageCallBack(OutputStream msgOutput) throws IOException
+    {
+    }
 
-	@Override
-	public OutputStream getOutputStream() {
-		return new SmartStream();
-	}
+    //    @Override
+    public OutputStream getOutputStream()
+    {
+        return new SmartStream();
+    }
 
-	@Override
-	public void dispose() {
-		// Doing nothing
-	}
-}
+    @Override
+    public void dispose()
+    {
+        // Doing nothing
+    }
 
-class MapperUnit {
-	int nMessageCounter;
-	Map<String, Object> headers;
-	IProcessingContext context;
-	
+    private static class SplitterCallback implements ISplitterCallback
+    {
+
+        IProcessingContext context;
+        IMessage           message;
+        int                nMessageCounter;
+        String             recordSeparator;
+        boolean            suppressEmptyMessages;
+
+        public SplitterCallback(IProcessingContext cntxt,
+                                IMessage messg)
+        {
+            super();
+            this.context = cntxt;
+            this.message = messg;
+        }
+
+        @Override
+        public void pushMessageCallBack(OutputStream msgOutput) throws IOException
+        {
+            // TODO Auto-generated method stub
+            try
+            {
+                if (!(msgOutput instanceof SmartStream))
+                {
+                    throw new IllegalArgumentException("SmartStream type expected for message creation");
+                }
+                else
+                {
+                    Map<String, Object> msgHeaders = message.getHeaders();
+                    if (!suppressEmptyMessages || ((SmartStream) msgOutput).getByteCount() > 0)
+                    {
+                        ++nMessageCounter;
+                        msgHeaders.put(MD_SPLIT_MESSAGE_ID, nMessageCounter);
+                        msgHeaders.put(RECORD_SEPARATOR, recordSeparator);
+                        msgHeaders.put(MD_SPLIT_MESSAGE_LENGTH, ((SmartStream) msgOutput).getByteCount());
+                        IMessage msgProcessed = context.getMessageFactory().createMessage(msgHeaders, ((SmartStream) msgOutput).getInputStream());
+                        ICompositeMessage msgExProcessed = context.getMessageFactory().createCompositeMessage(msgHeaders, msgProcessed);
+                        context.putResult(msgExProcessed);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw new IOException(e.getCause());
+            }
+
+        }
+
+        @Override
+        public OutputStream getOutputStream()
+        {
+            return new SmartStream();
+        }
+
+        public void setRrecordSeparator(String RS)
+        {
+            recordSeparator = RS;
+        }
+
+    }
 }
